fb78d71ed673d59fc4f4462dea63b85e64aeb2c4
ahgahg <aaa12345.59487@gamil.com>
2022-06-06 21:03:25 +0800

    seperate music and help cog to two files

--- cogs/Music.py
+++ cogs/Music.py
@@ -1,18 +1,17 @@
-from threading import local
 import discord
 from discord.ext import commands
 import youtube_dl
 from requests import get
-from discord.errors import Forbidden
 import asyncio
 from random import shuffle
+import json
 
 youtube_dl.utils.bug_reports_message = lambda: ''
 
 # Global variables
 ytdl_format_options = {
     'format': 'bestaudio/best',
-    'outtmpl': './audio files/%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'outtmpl': '../audio files/%(extractor)s-%(id)s-%(title)s.%(ext)s',
     'restrictfilenames': True,
     'noplaylist': True,
     'nocheckcertificate': True,
@@ -67,7 +66,7 @@ def check_queue(ctx):
         ctx.message.guild.voice_client.play(discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(search(url)[1], **FFMPEG_OPTS), 0.1), after=lambda x: check_queue(ctx))
 
 def local_queue_init(playlist):
-    with open(f"./playlists/{playlist}.txt", "r") as f:
+    with open(f"../playlists/{playlist}.txt", "r") as f:
         f.seek(0)
         
         global local_queue
@@ -78,25 +77,6 @@ def check_local_queue(ctx):
         source = local_queue.pop(0)
         
         ctx.message.guild.voice_client.play(discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(executable='ffmpeg.exe', source='./audio files/' + source.rstrip('\n')), volume=0.1), after=lambda x: check_local_queue(ctx))
-
-async def send_embed(ctx, embed):
-    """
-    Function that handles the sending of embeds
-    -> Takes context and embed to send
-    - tries to send embed in channel
-    - tries to send normal message when that fails
-    - tries to send embed private with information abot missing permissions
-    If this all fails: https://youtu.be/dQw4w9WgXcQ
-    """
-    try:
-        await ctx.send(embed=embed)
-    except Forbidden:
-        try:
-            await ctx.send("Hey, seems like I can't send embeds. Please check my permissions :)")
-        except Forbidden:
-            await ctx.author.send(
-                f"Hey, seems like I can't send any message in {ctx.channel.name} on {ctx.guild.name}\n"
-                f"May you inform the server team about this issue? :slight_smile: ", embed=embed)
 
 class YTDLSource(discord.PCMVolumeTransformer):
     def __init__(self, source, *, data, volume=0.1):
@@ -123,7 +103,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
 # Discord commands
 class Music(commands.Cog):
     """
-    Contains music commands
+    Musicですわ～
     """
 
     def __init__(self, bot):
@@ -161,13 +141,14 @@ class Music(commands.Cog):
             await ctx.send(f'**已加入撥放清單**：{search(url)[0]["title"]}')
 
     @commands.command(help="Download music from url")
+    @commands.is_owner()
     async def download(self, ctx, url, playlist):
         #download music from url
 
         await YTDLSource.from_url(url, loop=self.bot.loop)
 
         #wirte audio filename into "{playlist}.txt"
-        with open(f"./playlists/{playlist}.txt", 'a+') as f:
+        with open(f"../playlists/{playlist}.txt", 'a+') as f:
 
             f.seek(0)
 
@@ -185,6 +166,7 @@ class Music(commands.Cog):
                 await ctx.send("**Already exists!**")
 
     @commands.command(help='Play local playlist')
+    @commands.is_owner()
     async def play_local(self, ctx, playlist):
 
         if not ctx.message.author.voice:
@@ -312,119 +294,6 @@ class Music(commands.Cog):
         else:
             await ctx.send('沒有在播放任何音樂！')
 
-class Help(commands.Cog):
-    """
-    Sends this help message
-    """
-
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.command()
-    # @commands.bot_has_permissions(add_reactions=True,embed_links=True)
-    async def help(self, ctx, *input):
-        """Shows all modules of that bot"""
-	
-	    # !SET THOSE VARIABLES TO MAKE THE COG FUNCTIONAL!
-        prefix = '!'
-        version = '1.0.0'
-
-	    # setting owner name - if you don't wanna be mentioned remove line 49-60 and adjust help text (line 88) 
-        owner = 448437774656471050
-        owner_name = '蝦米碗糕#6182'
-
-        # checks if cog parameter was given
-        # if not: sending all modules and commands not associated with a cog
-        if not input:
-            # checks if owner is on this server - used to 'tag' owner
-            try:
-                owner = ctx.guild.get_member(owner).mention
-
-            except AttributeError as e:
-                owner = owner
-
-            # starting to build embed
-            emb = discord.Embed(title='Commands and modules', color=discord.Color.blue(),
-                                description=f'Use `{prefix}help <module>` to gain more information about that module '
-                                            f':smiley:\n')
-
-            # iterating trough cogs, gathering descriptions
-            cogs_desc = ''
-            for cog in self.bot.cogs:
-                cogs_desc += f'`{cog}` {self.bot.cogs[cog].__doc__}\n'
-
-            # adding 'list' of cogs to embed
-            emb.add_field(name='Modules', value=cogs_desc, inline=False)
-
-            # integrating trough uncategorized commands
-            commands_desc = ''
-            for command in self.bot.walk_commands():
-                # if cog not in a cog
-                # listing command if cog name is None and command isn't hidden
-                if not command.cog_name and not command.hidden:
-                    commands_desc += f'{command.name} - {command.help}\n'
-
-            # adding those commands to embed
-            if commands_desc:
-                emb.add_field(name='Not belonging to a module', value=commands_desc, inline=False)
-
-            # setting information about author
-            emb.add_field(name="About", value=f"壱百満天原サロメですわ～")
-            emb.set_footer(text=f"Bot is running {version}")
-
-        # block called when one cog-name is given
-        # trying to find matching cog and it's commands
-        elif len(input) == 1:
-
-            # iterating trough cogs
-            for cog in self.bot.cogs:
-                # check if cog is the matching one
-                if cog.lower() == input[0].lower():
-
-                    # making title - getting description from doc-string below class
-                    emb = discord.Embed(title=f'{cog} - Commands', description=self.bot.cogs[cog].__doc__,
-                                        color=discord.Color.green())
-
-                    # getting commands from cog
-                    for command in self.bot.get_cog(cog).get_commands():
-                        # if cog is not hidden
-                        if not command.hidden:
-                            emb.add_field(name=f"`{prefix}{command.name}`", value=command.help, inline=False)
-                    # found cog - breaking loop
-                    break
-
-            # if input not found
-            # yes, for-loops have an else statement, it's called when no 'break' was issued
-            else:
-                emb = discord.Embed(title="What's that?!",
-                                    description=f"I've never heard from a module called `{input[0]}` before :scream:",
-                                    color=discord.Color.orange())
-
-        # too many cogs requested - only one at a time allowed
-        elif len(input) > 1:
-            emb = discord.Embed(title="That's too much.",
-                                description="Please request only one module at once :sweat_smile:",
-                                color=discord.Color.orange())
-
-        else:
-            emb = discord.Embed(title="It's a magical place.",
-                                description="I don't know how you got here. But I didn't see this coming at all.\n"
-                                            "Would you please be so kind to report that issue to me on github?\n"
-                                            "https://github.com/nonchris/discord-fury/issues\n"
-                                            "Thank you! ~Chris",
-                                color=discord.Color.red())
-
-        # sending reply embed using our own function defined above
-        await send_embed(ctx, emb)
-
-
-# Bot settings
-intents = discord.Intents().all()
-bot = commands.Bot(
-    command_prefix=commands.when_mentioned_or("!"),
-    intents=intents
-)
 
 def setup(bot):
     bot.add_cog(Music(bot))
-    bot.add_cog(Help(bot))
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ token = os.getenv("token")
 intents = discord.Intents.all()
 bot = commands.Bot(
     command_prefix=commands.when_mentioned_or("!"),
-    description='眼睛好痛',
+    description='学校はマジで嫌いですわ～',
     intents=intents,
 )
 
@@ -23,6 +23,7 @@ async def on_ready():
     print(f'Logged in as {bot.user}')
     print('--------------------------')
 
-bot.load_extension('cogs')
+bot.load_extension('cogs/Music')
+bot.load_extension('cogs/Help')
 
 bot.run(token)
